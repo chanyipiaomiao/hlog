cff97b5dbe3e34704ec1e95ae21c33f48f699e25
helei <helei@zzpxx.com>
2023-12-07 21:25:43 +0800

    v2

--- doc.go
+++ doc.go
@@ -1,5 +1,1 @@
-// Copyright 2020 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package hlog // import "github.com/marmotedu/iam/pkg/log"
+package hlog
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/chanyipiaomiao/hlog
 
-go 1.17
+go 1.18
 
 require (
 	go.uber.org/zap v1.26.0
